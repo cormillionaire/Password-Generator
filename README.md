5f5610c7cb1241a7ca031c3137961a27115b530a
Cornelia Herman <corneliabherman@gmail.com>
2021-09-12 18:44:54 -0400

    updated to trigger prompts from button, added code to truely randomize trhe characters (vs going through the loop type by type until number characters met, and finally updated my readme.md

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-## User Story
+# 3-Password-Generator
+## What this is
+This is a javascript based passsword generation tool using a number of functions, methods, and other tools to generate a password that fit the user's needs
 
+This is for 2-Password-Generator
+
+![Password Generator](./assets/images/PasswordGeneratorPrompts.png)
+
+## User Stor
 ```
 AS AN employee with access to sensitive data
 I WANT to randomly generate a password that meets certain criteria
@@ -7,7 +14,6 @@ SO THAT I can create a strong password that provides greater security
 ```
 
 ## Acceptance Criteria
-
 ```
 GIVEN I need a new, secure password
 WHEN I click the button to generate a password
@@ -25,3 +31,20 @@ THEN a password is generated that matches the selected criteria
 WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page
 ```
+
+## Usage
+Using the existing methods available with javasript, the user is prompted to provide the number of characters they require for their password. It is written in the form of a functions so that it can easily be called again. 
+Once that is captured, the user provides what character types are needed for their password.
+With the number of characters needed and the types needed, the password can be generated using the button on the screen (code already existed)
+
+When creating the password and the funciton to put on the page, I made sure to use the appropriate var name. 
+
+
+## Opportunities
+An opportunity for the future is to allow the user to re-try within the run through if they provide a bad value (just allows the user know it was bad right now and allow to re-click the button)
+
+I  left console.log for each function so that I can easily see what is executing and what the output is. If this were something to deliver for actual usage, I would remove those to make the code base cleaner.
+
+## Credits
+Referenced most of out in-class javascript practice
+Referenced: https://www.coderrocketfuel.com/article/generate-a-random-letter-from-the-alphabet-using-javascript
--- script.js
+++ script.js
@@ -1,60 +1,61 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 let numberOfChar;
+let criteriaLower;
+let criteriaUpper;
+let criteriaNum;
+let criteriaSpecial;
 
 // Password criteria prompts 
 let setPasswordCriteria = function () {
-numberOfChar = window.prompt("Enter the number of characters in your password (range 8 to 128)");
-//Number of characters no less than 8 no more than 128
-if (numberOfChar < 8 || numberOfChar > 128) {
-  window.alert("You must enter a number between 8 and 128.");
-  setPasswordCriteria();
-} else if (isNaN(numberOfChar)) {
-  window.alert("You must enter a number.");
-  setPasswordCriteria();
-}
-// Letting user know they need to select at least one of the next prompts as true/yes
-window.alert("Select at least one of the following prompts. Answer with Y for Yes or N for No.");
+  numberOfChar = window.prompt("Enter the number of characters in your password (range 8 to 128)");
+  // Number of characters no less than 8 no more than 128
+  if (numberOfChar < 8 || numberOfChar > 128) {
+    window.alert("You must enter a number between 8 and 128.");
+  } else if (isNaN(numberOfChar)) {
+    window.alert("You must enter a number.");
+  }
+  // Letting user know they need to select at least one of the next prompts as true/yes
+  window.alert("Select at least one of the following prompts. Answer with Y for Yes or N for No.");
 }
 
-setPasswordCriteria();
+// Character types with  felixbility in answer y or Y for 'Yes' and n or N for 'No'
+let setPasswordCharType = function () {
+  criteriaLower = window.prompt("At least one lowercase letter?(Y/N)");
+  criteriaLower = criteriaLower.toUpperCase();
+  console.log(criteriaLower);
+  if (criteriaLower !== "Y" && criteriaLower !== "N") {
+    window.alert("Answer with Y for Yes or N for No.");
+  }
+  criteriaUpper = window.prompt("At least one uppercase letter?(Y/N)");
+  criteriaUpper = criteriaUpper.toUpperCase();
+  console.log(criteriaUpper);
+  if (criteriaUpper !== "Y" && criteriaUpper != "N") {
+    window.alert("Answer with Y for Yes or N for No.");
+  }
+  criteriaNum = window.prompt("At least one number?(Y/N)");
+  criteriaNum = criteriaNum.toUpperCase();
+  console.log(criteriaNum);
+  if (criteriaNum != "Y" && criteriaNum != "N") {
+    window.alert("Answer with Y for Yes or N for No.");
+  }
+  criteriaSpecial = window.prompt("At least one special character?(Y/N)");
+  criteriaSpecial = criteriaSpecial.toUpperCase();
+  console.log(criteriaSpecial);
+  if (criteriaSpecial != "Y" && criteriaSpecial != "N") {
+    window.alert("Answer with Y for Yes or N for No.");
+  } else {
+    window.alert("You've selected " + criteriaLower + " for Lower " + criteriaUpper + " for Upper " + criteriaNum + " for Number " + criteriaSpecial + "for Special Character");
+  }
+  // Validation at least one of these was selected
+  if (criteriaLower === "N" && criteriaUpper === "N" && criteriaNum === "N" && criteriaSpecial === "N") {
+    window.alert("You must select at least one character criteria for your password")
+  }
+}
 
-let criteriaLower = window.prompt("At least one lowercase letter?(Y/N)");
-criteriaLower = criteriaLower.toUpperCase();
-console.log(criteriaLower);
-if (criteriaLower !== "Y" && criteriaLower !== "N") {
-  window.alert("Answer with Y for Yes or N for No.");
-}
-let criteriaUpper = window.prompt("At least one uppercase letter?(Y/N)");
-criteriaUpper = criteriaUpper.toUpperCase();
-console.log(criteriaUpper);
-if (criteriaUpper !== "Y" && criteriaUpper != "N") {
-  window.alert("Answer with Y for Yes or N for No.");
-}
-let criteriaNum = window.prompt("At least one number?(Y/N)");
-criteriaNum = criteriaNum.toUpperCase();
-console.log(criteriaNum);
-if (criteriaNum != "Y" && criteriaNum != "N") {
-  window.alert("Answer with Y for Yes or N for No.");
-}
-let criteriaSpecial = window.prompt("At least one special character?(Y/N)");
-criteriaSpecial = criteriaSpecial.toUpperCase();
-console.log(criteriaSpecial);
-if (criteriaSpecial != "Y" && criteriaSpecial != "N") {
-  window.alert("Answer with Y for Yes or N for No.");
-} else {
-  window.alert("You've selected " + criteriaLower + " for Lower " + criteriaUpper + " for Upper " + criteriaNum + " for Number " + criteriaSpecial + "for Special Character");
-}
-
-// validation at least one of each was selected
-if (criteriaLower === "N" && criteriaUpper === "N" && criteriaNum === "N" && criteriaSpecial === "N") {
-  window.alert("You must select at least one character criteria for your password")
-}
-console.log("Continue");
-
-// Write password to the #password input
+// Generate character based on criteria
 let generateCharacterUpper = function () {
-  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   return alphabet[Math.floor(Math.random() * alphabet.length)];
 }
 
@@ -66,25 +67,28 @@ let generateCharacterLower = function () {
 console.log(generateCharacterLower());
 
 let generateCharacterSpecial = function () {
-  const special = "!@#$%^&*"
+  const special = "!@#$%^&*";
   return special[Math.floor(Math.random() * special.length)];
 }
 console.log(generateCharacterSpecial());
 
 let generateCharacterNum = function () {
-  const number = "1234567890"
+  const number = "1234567890";
   return number[Math.floor(Math.random() * number.length)];
 }
 console.log(generateCharacterNum());
 
+// Write password 
 let generatePassword = function () {
-  //take the number of characters
+  console.log("You've selected " + criteriaLower + " for Lower; " + criteriaUpper + " for Upper; " + criteriaNum + " for Number; " + criteriaSpecial + " for Special Character")
+  // Take the number of characters
   let buildPassword = "";
+  // Loop through each of the character types, adding relevant characters until numberOfChar reached or slightly passed
   for (var i = 0; i < numberOfChar; i++) {
     if (criteriaLower === "Y") {
       buildPassword = buildPassword + generateCharacterLower();
     }
-    if (criteriaLower === "Y") {
+    if (criteriaUpper === "Y") {
       buildPassword = buildPassword + generateCharacterUpper();
     }
     if (criteriaSpecial === "Y") {
@@ -94,11 +98,34 @@ let generatePassword = function () {
       buildPassword = buildPassword + generateCharacterNum();
     }
   }
-  return buildPassword.slice(0, numberOfChar);
+  // Make sure to trim down the password to correct size and set buildpwd to the trimmed down pwd
+  buildPassword = buildPassword.slice(0, numberOfChar);
+  console.log(buildPassword);
+  // set the jumblePassword var to ""
+  let jumblePassword = "";
+  // start a for loop to loop through the length of buildpwd
+  for (var i = 0; i < numberOfChar; i++) {
+    let buildPasswordIndex = Math.floor(Math.random() * buildPassword.length);
+    jumblePassword = jumblePassword + buildPassword[buildPasswordIndex];
+    // To make sure the character isnt repeated when randomly picking out the value
+    // To make sure the array selection stays within the correct range, need this statement
+    if (buildPassword.length === buildPasswordIndex) {
+      buildPassword = buildPassword.slice(0, buildPasswordIndex)
+      // This works for everything else by triming and keeping what is before and what is after what was pulled by the index.
+    } else {
+      buildPassword = buildPassword.slice(0, buildPasswordIndex) + buildPassword.slice(buildPasswordIndex + 1, buildPassword.length)
+    }
+    console.log("bp: " + buildPassword);
+    console.log("jum: " + jumblePassword);
+  }
 
+  return jumblePassword;
 }
 
+// Write password to the #password input
 function writePassword() {
+  setPasswordCriteria();
+  setPasswordCharType();
   let password = generatePassword();
   console.log(password);
   let passwordText = document.querySelector("#password");
